2cc6367788d83f5a185c3c79a99da81cdd1fe4ad
Ayush Rusiya <ayushrusiya47@gmail.com>
2021-07-08 13:37:33 +0530

    bug fix

--- routes/userData.js
+++ routes/userData.js
@@ -3,11 +3,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCode, changeCode } = require("../contollers/userData"); //Getting functions
+const { getData, changeCode } = require("../contollers/userData"); //Getting functions
 const { verifyToken } = require("../middlewares/authMiddleware"); //Middleware for verifying token
 
 //Requests
-router.get("/getCode", verifyToken, getCode);
+router.get("/getData", verifyToken, getData);
 router.put("/changeCode", verifyToken, changeCode);
 
 module.exports = router;
