4b67f3faf667a0912eebede5baa0251799b134e4
Ayush Rusiya <76245068+ayushrusiya47@users.noreply.github.com>
2021-07-09 23:13:48 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { signUp, signIn } = require("../contollers/auth"); //Getting functions
+const { signUp, signIn, changePassword } = require("../contollers/auth"); //Getting functions
+const { verifyToken } = require("../middlewares/authMiddleware");
 
 //Requests
 router.post("/signup", signUp);
 router.post("/signin", signIn);
+router.post("/changePassword", verifyToken, changePassword);
 
 module.exports = router;
